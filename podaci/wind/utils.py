3dce22e3f54406869b472b7c3ef54df7abf48f00
LDH <343091787@qq.com>
2017-10-13 15:00:44 +0800

    wind接口现在支持获取多只股票多个因子，并且可以同时获得对应万德行业的因子

--- podaci/wind/utils.py
+++ podaci/wind/utils.py
@@ -19,10 +19,17 @@ def code_2_wind_symbol(code):
         wind_symbol = code + '.SZ'
     return wind_symbol
 
+def dict_2_str(d):
+    final_str = ''
+    for key,value in d.items():
+        final_str += str(key) + '=' + str(value) + ';'
+    return final_str[:-1]
+
 wind_symbol_2_code = np.frompyfunc(wind_symbol_2_code,1,1)
 code_2_wind_symbol = np.frompyfunc(code_2_wind_symbol,1,1)
 
 if __name__ == '__main__':
     universe = ['600340','000001']
     universe_wind = code_2_wind_symbol(universe)
-
+    d = {'industryType':1}
+    a = dict_2_str(d)
--- podaci/wind/wind.py
+++ podaci/wind/wind.py
@@ -8,12 +8,12 @@ Created on Thu Oct 12 15:53:38 2017
 # wind.py
 import pandas as pd
 from WindPy import *
-from utils import wind_symbol_2_code,code_2_wind_symbol
+from utils import wind_symbol_2_code,code_2_wind_symbol,dict_2_str
 
 w.start()
-def get_stocks_factors(universe,factors,trade_date):
+def get_factors(universe,factors,if_convert = True,**options):
     '''
-    获取多个股票在某一日的多个因子数据。
+    获取多个因子数据。
     
     Parameters
     ----------
@@ -23,30 +23,42 @@ def get_stocks_factors(universe,factors,trade_date):
         'pe_ttm,pb_mrq'
     trade_date
         '20171011'
+    if_convert
+        是否将universe转换成wind代码,默认为True
+    options
+        其他参数
         
     Returns
     --------
     '''
-    universe_wind = code_2_wind_symbol(universe)
-    universe_wind = ','.join(universe_wind)
-    data = w.wss(universe_wind,factors,'tradeDate=%s'%trade_date)
+    options = dict_2_str(options)
+    if if_convert:
+        universe_wind = code_2_wind_symbol(universe)
+        universe_wind = ','.join(universe_wind)
+    else:
+        universe_wind = ','.join(universe)
+    data = w.wss(universe_wind,factors,options)
     df = pd.DataFrame(data.Data,index = data.Fields,columns = universe).T
     return df
     
-def get_stocks_industries(universe):
-    '''
-    获取多个股票对应的万德行业。
-    '''
-    pass
 
-def get_stocks_factors_with_industry(universe,factors,trade_date):
+def get_stocks_factors_with_industry(universe,factors,**options):
     '''
-    获取多个股票
+    获取多个股票因子及对应的行业因子(万德).
     '''
-    pass
+    factors_ = factors + ',indexcode_wind'
+    data = get_factors(universe,factors_,True,**options)
+    index_codes = list(set(data['INDEXCODE_WIND'].dropna().values)) # 避免bug
+    index_data = get_factors(index_codes,factors,False,**options)
+    columns = index_data.columns
+    industry_columns = [each + '_industry' for each in columns]
+    index_data.columns = pd.Index(industry_columns)
+    data = data.join(index_data,on = 'INDEXCODE_WIND')
+    return data
 
 if __name__ == '__main__':
-    universe = ['600340','000001']
-    factors = 'pe_ttm,pb_mrq'
-    trade_date = '20171011'
-    data = get_stocks_factors(universe,factors,trade_date)
+    universe = ['600340','000001','600000']
+    factors = 'pe_ttm'
+    trade_date = '20171012'
+    data = get_factors(['882007.WI'],factors,False,tradeDate = '20171012', industryType = 2)
+    a = get_stocks_factors_with_industry(universe,factors,tradeDate = '20171012',industryType = 1)
